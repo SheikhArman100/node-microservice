159e3a8843df62221d3798c4aa846957045775c8
Sheikh Arman <sheikharman@softograph.com>
2025-10-23 18:02:47 +0600

    fix order-service

--- order-service/src/config/index.ts
+++ order-service/src/config/index.ts
@@ -15,11 +15,15 @@ validateEnvVar('NODE_ENV', process.env.NODE_ENV);
 validateEnvVar('PORT', process.env.PORT);
 validateEnvVar('RABBITMQ_URL', process.env.RABBITMQ_URL);
 validateEnvVar('POSTGRESQL_URI', process.env.POSTGRESQL_URI);
+validateEnvVar('SOFTOGRAPH_EMAIL', process.env.SOFTOGRAPH_EMAIL);
+validateEnvVar('SOFTOGRAPH_PASS', process.env.SOFTOGRAPH_PASS);
 
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   rabbitmq_url: process.env.RABBITMQ_URL,
   postgresql_uri: process.env.POSTGRESQL_URI,
+   softograph_email: process.env.SOFTOGRAPH_EMAIL,
+  softograph_pass: process.env.SOFTOGRAPH_PASS,
   
 };
